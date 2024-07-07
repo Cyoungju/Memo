24bdc9a4e43fb30848e1e3247602dba0be10a974
Cyoungju <dudwn3528@gmail.com>
2024-07-07 23:11:16 +0900

    Query methods

--- src/main/java/com/sparta/demo/repository/MemoRepository.java
+++ src/main/java/com/sparta/demo/repository/MemoRepository.java
@@ -2,9 +2,13 @@ package com.sparta.demo.repository;
 
 import com.sparta.demo.entity.Memo;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 
 public interface MemoRepository extends JpaRepository<Memo, Long> {
+
+    List<Memo> findAllByOrderByModifiedAtDesc();
+    //ModifiedAt 정렬해서 내림차순으로 불러오기
 
 }
